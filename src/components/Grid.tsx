22fb50a722ec04e3f6ffcfc3c52e56c8ba57e4da
mkekovic <mkekovic@islander.tamucc.edu>
2023-02-04 17:19:10 -0600

    Added Components For Grid

---
+++ src/components/Grid.tsx
@@ -0,0 +1,99 @@
+import react, { useState, useEffect, useMemo, useCallback } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
+import Spinner from './Spinner';
+// import { useIsAuthenticated } from "@azure/msal-react";
+import UnauthorizedContent from "./UnauthorizedContent";
+
+export default function Grid(props: any) {
+    const [isLoading, setIsLoading] = useState(true)
+    const [rowData, setRowData] = useState([]);
+    const [columnDefs, setColumnDefs] = useState([{}]);
+    let isAuth: any = false;//useIsAuthenticated();
+
+    var myHeaders = new Headers();
+    myHeaders.append("authToken", "abc123");
+    const renderTableContent = () => {
+        if (isLoading) {
+            return (
+                <Spinner />
+            )
+        } else if (isAuth || props.isStructured) { // for security of auth
+            return (
+                <div className='ag-theme-alpine h-96 w-7/12' >
+                    <AgGridReact
+                        onCellClicked={rowClickedListener}
+                        rowData={rowData}
+                        columnDefs={columnDefs}
+                        defaultColDef={defaultColDef}
+                        rowSelection='multiple'
+                        animateRows={true}
+                    />
+                </div>
+            )
+        }
+    }
+
+    const generateColumnDefs = (obj: any) => {
+        let columnData = [];
+        for (const key in obj) {
+            columnData.push({ field: `${key}` })
+        }
+        return (columnData);
+    }
+
+    const rowClickedListener = useCallback((e: any) => {
+        console.log('cell Clicked: ', e)
+    }, [])
+
+    const defaultColDef = useMemo(() => ({
+        sortable: true,
+        filter: true
+    }), [])
+
+    useEffect(() => {
+        fetch(props.url, props.isStructured ? {} : isAuth ? {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        } : {}).then(result => result.json()).then(rowData => {
+            if (rowData?.message == 'Unauthorized') {
+                setIsLoading(false)
+                throw new Error('Unauthorized');
+            }
+            let resultData = props.isStructured ? rowData : rowData.Items
+            setColumnDefs(generateColumnDefs(resultData[0]))
+            setRowData(resultData)
+            setIsLoading(false)
+        }).catch((err) => {
+            console.log(err);
+        });
+    }, [])
+
+    useEffect(() => {
+        isAuth && setIsLoading(true)
+        fetch(props.url, props.isStructured ? {} : isAuth ? {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        } : {}).then(result => result.json()).then(rowData => {
+            if (rowData?.message == 'Unauthorized') {
+                setIsLoading(false)
+                throw new Error('Unauthorized');
+            }
+            let resultData = props.isStructured ? rowData : rowData.Items
+            setColumnDefs(generateColumnDefs(resultData[0]))
+            setRowData(resultData)
+            setIsLoading(false)
+        }).catch((err) => {
+            console.log(err)
+        });
+    }, [isAuth])
+
+    return (
+        <div className='display flex h-80 w-full justify-center items-center flex-col my-4'>
+            {!isAuth && !props.isStructured ? <UnauthorizedContent /> : renderTableContent() }
+        </div>
+    )
+}
---
+++ src/components/UnauthorizedContent.tsx
@@ -0,0 +1,30 @@
+export default function UnauthorizedContent() {
+    return (
+      <div
+    className="
+      flex
+      items-center
+      justify-center
+      w-screen
+      h-screen
+      bg-gradient-to-r
+      from-indigo-600
+      to-blue-400
+    "
+  >
+    <div className="px-40 py-5 bg-white rounded-md shadow-xl">
+      <div className="flex flex-col items-center">
+        <h1 className="font-bold text-blue-600 text-9xl">403</h1>
+  
+        <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+          <span className="text-red-500">Oops!</span> Unauthorized Access
+        </h6>
+  
+        <p className="mb-8 text-center text-gray-500 md:text-lg">
+          Pelase sign in to see this table.
+        </p>
+      </div>
+    </div>
+  </div>
+    )
+  }
