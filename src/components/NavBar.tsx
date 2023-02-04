aa779ea6b2b7a719f1a9625ab5007e4f40ead8dc
mkekovic <mkekovic@islander.tamucc.edu>
2023-02-04 15:48:07 -0600

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -23,7 +23,7 @@ const NavBar = () => {
                         </li>
 
                         <li>
-                            <Link as="/about" href="/About" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100  md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white ">About</Link>
+                            <Link as="/about" href="/about" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100  md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white ">About</Link>
                         </li>
 
                         <li>
