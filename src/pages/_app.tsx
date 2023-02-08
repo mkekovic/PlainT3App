7f58d0549148a63955979dde5d6342cbef309e7d
mkekovic <mkekovic@islander.tamucc.edu>
2023-02-08 09:15:42 -0600

    Updated App to Contain MSAL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Layout } from '../components/index';
+import { PublicClientApplication, EventType } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
 
 import { api } from "../utils/api";
 
@@ -11,13 +13,29 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
-  );
-};
+  const pca = new PublicClientApplication({
+    auth: {
+      clientId: 'daccbcbd-9465-4cbd-a810-439451287747',
+      authority: 'https://login.microsoftonline.com/997bb24e-f4ba-452f-8f21-13714e0a657b',
+      redirectUri: '/',
+    }
+  });
 
+  pca.addEventCallback((event: any) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS) {
+      console.log(event);
+      pca.setActiveAccount(event.payload.account)
+    };
+  });
+    
+  return (
+    <MsalProvider instance={pca}>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </MsalProvider>
+  );
+}
 export default api.withTRPC(MyApp);
